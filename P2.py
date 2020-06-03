7295402ba990636d1debadbc30756f93cd5e0352
Marina Neumann <marinaneumann2468@yahoo.com>
2020-06-03 11:47:53 -0700

    Added some user structure

--- P2.py
+++ P2.py
@@ -1,11 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
     print("Programming Assignment #3")
     print("By Marina Neumann")
     print("Spring 2020 Machine Learning")
+
     dataLoad()
-    kMeans()
+    choice = ''
+    while choice != 'e':
+        choice = input("Enter 'k' for K-Means clustering, 'f' for Fuzzy C-Means, and 'e' for exit: ")
+        print("\n")
+        if choice == 'k':
+            print("K-Means Clustering")
+            kMeansAlg()
+        elif choice == 'f':
+            fuzzyCMeansAlg()
+
+
 
 def dataLoad():
     global data, dX, dY, dataNum, fNum
@@ -14,10 +26,27 @@ def dataLoad():
     dY = data[:,1]
     dataNum = data.shape[0]
     fNum = data.shape[1]
+    plt.scatter(dX, dY, c="blue")
+    plt.show()
+
+def kMeansAlg():
+
+    k = int(input("What is the value of K?"))
+    r = int(input("How many times would you like to run?"))
+
+    for i in range(r):
+        print("wassssup")
 
 
-def kMeans():
-    print("sup")
+def fuzzyCMeansAlg():
+    print("hi")
+
+class kMeans:
+    def __init__(self,k):
+        self.k = k
 
 
+class fuzzyC:
+    def __init__(self, k):
+        self.k = k
 main()
