7b6a88bb1e2e649ddb51b837d079b065f54a7ea2
Marina Neumann <marinaneumann2468@yahoo.com>
2020-06-13 21:36:16 -0700

    Changes to file as adding comments, removing unnecessary or commented out code from diferent versions, all done?

--- P3.py
+++ P3.py
@@ -30,6 +30,7 @@ def dataLoad():
     # plt.scatter(dX, dY, c="blue")
     # plt.show()
 
+#Very important function for clustering algorithms below
 def euclideanDistance(X, Y):
         return (sum((X-Y)**2))**0.5
 
@@ -37,12 +38,12 @@ def kMeansAlg():
 
     k = int(input("What is the value of K?")) #Takes user input of k
     kCluster = kMeans(k, dataNum, fNum,data) #Creates kCluster object
+    n = int(input("How many iterations?: "))  # Asks user for how many iterations
 
     rseed =2
     rng =np.random.RandomState(rseed)
     i = rng.permutation(data.shape[0])[:k] # Creates random indexes to assign centers
     kCluster.centers = data[i] #Creates centers at those random data points
-    n = int(input("How many iterations?: ")) #Asks user for how many iterations
     squaredSums = []
 
     for z in range(n):
@@ -52,28 +53,18 @@ def kMeansAlg():
 
         if np.all(kCluster.centers == kCluster.prev_centers):
             break
-
+        #Sum of square values appened to array outside loop to be evaluated later for lowest SSS.
         ss = kCluster.squareSums()
         squaredSums =np.append(squaredSums, ss)
 
-        # colors = ['green', 'purple', 'blue', 'orange', 'brown', 'pink', 'black', 'violet', 'teal', 'tomato', 'maroon',
-        #           'olive', 'gold']
-        # for c in kCluster.centers:squaredSums = []
-        #     plt.scatter(c[0], c[1], marker='*', color="red", s=200)
-        # for d in range(kCluster.k):
-        #     color = colors[d]
-        #     num = len(kCluster.clusters[d]) / 2
-        #     kCluster.clusters[d] = np.array_split(kCluster.clusters[d], num)
-        #     for k in kCluster.clusters[d]:
-        #         plt.scatter(k[0], k[1], marker="o", color=color, s=2)
-        # plt.title('K-Means Algorithm', size=16)
-        # plt.show()
 
+    #Code for sum of squares results from different clustering iterations w/ different K values
     sumMin = np.argmin(squaredSums)
     print("Solution with lowest sum was at itertion:", sumMin)
     mSS = squaredSums[sumMin]
     print("Lowest sum was :", mSS)
 
+    #Code used to graph clustering of algorithm
     colors = ['green', 'purple', 'blue', 'orange', 'brown', 'pink', 'black', 'violet', 'teal','tomato','maroon', 'olive', 'gold']
 
     for c in kCluster.centers:
@@ -89,6 +80,7 @@ def kMeansAlg():
     plt.title('K-Means Algorithm' ,size=16)
     plt.show()
 
+#Kmeans class and functions
 class kMeans:
     def __init__(self,k, dataNum, fNum, data):
         self.k = k
@@ -96,10 +88,7 @@ class kMeans:
         self.features = fNum
         self.data = data
 
-
-    # def euclideanDistance(self,X, Y):
-    #     return (sum((X-Y)**2))**0.5
-
+    #Responsible for assigning data to different clusters
     def assign(self, data):
         for i in range(self.k): #Arranges for K different clusters
             self.clusters[i] = []
@@ -109,10 +98,12 @@ class kMeans:
             self.clusters[cluster] =np.append(self.clusters[cluster],f)
         self.prev_centers = dict(self.centers)
 
+    #Calculates centers for each clusters
     def calcCenters(self):
         for c in self.clusters:
             self.centers[c] = np.mean(self.clusters[c], axis =0)
 
+    #Calculates square sums for value at K, with state of clusters and centers at that iteration.
     def squareSums(self ):
         sums = 0
         for i in range(self.k):
@@ -122,7 +113,7 @@ class kMeans:
             sums += cSUM
         return sums
 
-
+#Function to declare fuzzy C-means functions and such
 def fuzzyCMeansAlg():
     print("hi")
     k = int(input("What is the value of K?"))  # Takes user input of k
@@ -131,7 +122,7 @@ def fuzzyCMeansAlg():
     n = int(input("How many iterations?: "))
     squaredSums = []
     for i in range(n):
-        print("WAZZZZZZUP?")
+
         fMeans.clusters = {}
         fMeans.computeCenter()
         fMeans.updateMembership()
@@ -166,6 +157,7 @@ def fuzzyCMeansAlg():
     plt.title('Fuzzy C-Means Algorithm', size=16)
     plt.show()
 
+#Fuzzy C class and functions
 class fuzzyC:
     def __init__(self, k, dataNum, fNum, data):
         self.k = k
@@ -175,15 +167,13 @@ class fuzzyC:
         self.m = 2  #Default set value,for fuzzier parameter
         self.centers ={}
 
-
+    #Function to computer centers for each cluster, using fuzzy c centers equation
     def computeCenter(self):
-        print("WAZZUP")
         self.old_centers = self.centers
         wkM = np.power(self.membership, self.m)
         self.centers = np.transpose(np.matmul(self.data.T, wkM) / np.sum(wkM, axis=0))
 
-
-
+    #Function to update membership of each data and its relation to specific cluster
     def  updateMembership(self):
         print("Blah Blah Blah ")
         self.old_membership = self.membership
@@ -194,14 +184,10 @@ class fuzzyC:
                 den = sum([pow(float(distances[j] / distances[c]), d) for c in range(self.k)])
                 self.membership[i][j] = float(1 / den)
 
-
+    #Function for the clustering of data depending on data X to cluster C
     def clustering(self):
         for i in range(self.k):
             self.clusters[i] = []
-
-        # for i in range(self.dNum):
-        #     max_val, idx = max((val, idx) for (idx, val) in enumerate(self.membership[i]))
-        #     self.clusters = np.append(self.clusters, idx)
 
         for f in self.data:
             distances = [euclideanDistance(f, self.centers[c]) for c in range(self.k)]  # calculates distances in data
@@ -209,8 +195,7 @@ class fuzzyC:
             self.clusters[cluster] = np.append(self.clusters[cluster], f)
         self.prev_centers = dict(self.centers)
 
-
-
+    #Function to calculate sum of squares for Fuzzy C clusters and centers at a specific iteration
     def sumSquares(self):
         print("BLOOOPPPP?")
         sums = 0
@@ -222,4 +207,4 @@ class fuzzyC:
         print("These are the sums?:", sums)
         return sums
 
-main()
+main() #Function call which puts everything into motion..
